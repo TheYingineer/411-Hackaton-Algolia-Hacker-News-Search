e5be40377fec77421c61ed966b2ab76973d9aad5
unknown <kaitlandevans@gmail.com>
2022-08-09 17:33:34 -0600

    Header Styles Continued

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -8,6 +8,16 @@ function SearchBar({ placeholder, data }) {
         <input type="text" placeholder={placeholder}></input>
         <div ClassName="searchIcon"></div>
       </div>
+      {/* <div ClassName="SortByBar">
+        <h4>Search</h4>
+        <input list="Stories">
+          {/* <option value="All"/> */}
+        {/* </input>
+        <h4>by</h4>
+        <input type=""></input>
+        <h4>for</h4>
+        <input type=""></input>
+      </div> */} 
       <div ClassName="dataResults"></div>
     </div>
   );
