8838cad327b7af8bae90b82b8165e384d751e3a9
KHONGTUB <jkojeda12@gmail.com>
2022-08-09 15:21:35 -0500

    Added functional search

---
+++ src/Components/List.js
@@ -0,0 +1,30 @@
+import { React } from "react";
+
+function List({ news, inputText }) {
+  const filtered = news.filter((element) => {
+    if (inputText === "") {
+      return element;
+    } else {
+      return element.title.toLowerCase().includes(inputText);
+    }
+  });
+
+  return (
+    <ol>
+      {filtered.map((article, index) => (
+        <li key={article.objectID + index}>
+          <h2>
+            <div className="title">{article.title}</div>
+            <div className="url">({article.url})</div>
+          </h2>
+          <p>
+            {article.points} points | {article.author} |{article.created_at} |{" "}
+            {article.num_comments} comments
+          </p>
+        </li>
+      ))}
+    </ol>
+  );
+}
+
+export default List;
