859437deccc61f02de67cb2f5bddd5dbc7a3009f
KHONGTUB <jkojeda12@gmail.com>
2022-08-11 17:58:50 -0500

    Added links to title and momentJS

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ function App() {
     fetch("http://hn.algolia.com/api/v1/search?tags=front_page")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data.hits);
         setNews(data.hits);
+        console.log(data.hits);
       });
   }, []);
 
@@ -47,28 +47,6 @@ function App() {
             <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z"></path>
           </svg>
           <h2>Settings</h2>
-          {/* <button onClick={filteredSearch}>search</button> */}
-          {/* <ol>
-            {news.map((article, index) => {
-              return (
-                <li key={article.objectID + index}>
-                  <h2>
-                    <div className="title">{article.title}</div>
-                    <div className="url">({article.url})</div>
-                  </h2>
-                  <p>
-                    {article.points} points | {article.author} |
-                    {article.created_at} | {article.num_comments} comments
-                  </p>
-                </li>
-              );
-            })}
-          </ol> */}
-
-          {/* <SearchBar
-            handleChange={handleInput}
-            placeholder="Search stories by title, url, or author"
-          /> */}
         </header>
         <List news={news} inputText={inputText} />
       </div>
